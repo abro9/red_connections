3814d940ee5f575739a0e9ce21710350ac77e0ae
abro9 <aab2212@columbia.edu>
2018-05-30 11:39:46 -0400

    functions to collect some subreddit data

--- read.py
+++ read.py
@@ -4,11 +4,43 @@ import praw
 
 reddit = praw.Reddit('bot1')
 
-subreddit = reddit.subreddit("learnpython")
+redditor_dict = {}
 
-for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    print("Test: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("-------------------------------\n")
+def get_submission_subreddit_dict(username):
+    subreddit_dict = {}
+
+    for submission in reddit.redditor(username).submissions.new(limit=None):
+        sub_name = submission.subreddit.display_name
+        if sub_name in subreddit_dict.keys():
+            subreddit_dict[sub_name] += 1
+        else:
+            subreddit_dict[sub_name] = 1
+
+    return subreddit_dict
+
+def get_comment_subreddit_dict(username):
+    subreddit_dict = {}
+
+    for comment in reddit.redditor(username).comments.new(limit=None):
+        #print(comment.subreddit)
+        sub_name = comment.subreddit.display_name
+        if sub_name in subreddit_dict.keys():
+            subreddit_dict[sub_name] += 1
+        else:
+            subreddit_dict[sub_name] = 1
+
+    return subreddit_dict
+
+def build_user_dict(username):
+    user_dict = {}
+    user_dict['submissions'] = get_submission_subreddit_dict(username)
+    user_dict['comments'] = get_comment_subreddit_dict(username)
+
+    return user_dict
+
+user = 'raymincer'
+
+redditor_dict[user] = build_user_dict(user)
+print('Submissions: ' + str(redditor_dict[user]['submissions']))
+print('Comments: ' + str(redditor_dict[user]['comments']))
 

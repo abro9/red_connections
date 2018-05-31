e5438744a17475fc82ea0a51d4d45ad607843901
abro9 <aab2212@columbia.edu>
2018-05-31 11:42:17 -0400

    successful data capture, small update to collection script

--- read.py
+++ read.py
@@ -1,6 +1,11 @@
 #!./venv/bin/python3
 
 import praw
+import prawcore
+import json
+
+data_input_path = './data/usernames_POPULAR_1000.txt'
+data_output_path = './usernames_POPULAR_1000.json'
 
 reddit = praw.Reddit('bot1')
 
@@ -9,24 +14,30 @@ redditor_dict = {}
 def get_submission_subreddit_dict(username):
     subreddit_dict = {}
 
-    for submission in reddit.redditor(username).submissions.new(limit=None):
-        sub_name = submission.subreddit.display_name
-        if sub_name in subreddit_dict.keys():
-            subreddit_dict[sub_name] += 1
-        else:
-            subreddit_dict[sub_name] = 1
+    try:
+        for submission in reddit.redditor(username).submissions.new(limit=None):
+            sub_name = submission.subreddit.display_name
+            if sub_name in subreddit_dict.keys():
+                subreddit_dict[sub_name] += 1
+            else:
+                subreddit_dict[sub_name] = 1
+    except prawcore.exceptions.NotFound:
+        pass
 
     return subreddit_dict
 
 def get_comment_subreddit_dict(username):
     subreddit_dict = {}
 
-    for comment in reddit.redditor(username).comments.new(limit=None):
-        sub_name = comment.subreddit.display_name
-        if sub_name in subreddit_dict.keys():
-            subreddit_dict[sub_name] += 1
-        else:
-            subreddit_dict[sub_name] = 1
+    try:
+        for comment in reddit.redditor(username).comments.new(limit=None):
+            sub_name = comment.subreddit.display_name
+            if sub_name in subreddit_dict.keys():
+                subreddit_dict[sub_name] += 1
+            else:
+                subreddit_dict[sub_name] = 1
+    except prawcore.exceptions.NotFound:
+        pass
 
     return subreddit_dict
 
@@ -37,9 +48,35 @@ def build_user_dict(username):
 
     return user_dict
 
-user = 'raymincer'
+def build_user_list(input_file):
+    user_list = []
 
-redditor_dict[user] = build_user_dict(user)
-print('Submissions: ' + str(redditor_dict[user]['submissions']))
-print('Comments: ' + str(redditor_dict[user]['comments']))
+    with open(input_file, 'r') as f:
+        for line in f:
+            user_list.append(line.strip())
+
+    return user_list
+
+if __name__ == "__main__":
+
+# user = 'raymincer'
+
+# redditor_dict[user] = build_user_dict(user)
+# print('Submissions: ' + str(redditor_dict[user]['submissions']))
+# print('Comments: ' + str(redditor_dict[user]['comments']))
+
+    redditor_dict = {}
+    user_list = build_user_list(data_input_path)
+
+    for user in user_list:
+        redditor_dict[user] = build_user_dict(user)
+
+        dict_len = len(redditor_dict)
+        user_list_len = len(user_list)
+
+        if dict_len % 50 <= 1:
+            print(str(dict_len * 100 / user_list_len) + '%')
+
+    with open(data_output_path, 'w') as f:
+        json.dump(redditor_dict, f)
 

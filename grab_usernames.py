7f56bf581829cded492e5b1c420ba10cba180e06
Alexander Brown <aab2212@columbia.edu>
2018-06-01 03:04:42 +0000

    improved output filenaming for grab_usernames

--- grab_usernames.py
+++ grab_usernames.py
@@ -1,13 +1,14 @@
 #!./venv/bin/python3
 
 import praw
+import os import listdir
 
 SUB = 'All'
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit(SUB)
 
 user_set = set()
-num_users = 10000
+num_users = 2000
 
 for submission in subreddit.stream.submissions():
 	if submission.author != None:
@@ -27,7 +28,14 @@ for submission in subreddit.stream.submissions():
 	if set_len > num_users:
 		break
 
-with open('usernames_' + SUB.upper() + '_' + str(num_users) + '.txt', 'w') as f:
+output_num = 0
+dir_list = listdir('./')
+output_filename = 'usernames_{}_{}_{}.txt'.format(SUB.upper(), num_users, output_num)
+
+while output_filename in dir_list:
+    output_filename = 'usernames_{}_{}_{}.txt'.format(SUB.upper(), num_users, output_num + 1)
+
+with open(output_filename, 'w') as f:
 	for username in user_set:
 		f.write(username + '\n')
 

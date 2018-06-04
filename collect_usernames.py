f80286ef37ed280f4c9c82aa538b325480d08e30
Alexander Brown <aab2212@columbia.edu>
2018-06-04 05:09:25 +0000

    script that collects all usernames

---
+++ collect_usernames.py
@@ -0,0 +1,22 @@
+#!./venv/bin/python3
+
+from os import listdir
+
+user_set = set()
+
+input_path = '/home/pi/projects/red_connections/data/usernames/'
+output_path = '/home/pi/projects/red_connections/data/all_usernames.txt'
+
+dir_list = listdir(input_path)
+
+for user_file in dir_list:
+    current_input_file = '/home/pi/projects/red_connections/data/usernames/{}'.format(user_file)
+    
+    with open(current_input_file, 'r') as f:
+        for line in f:
+            user_set.add(line)
+
+with open(output_path, 'w') as f:
+	for username in user_set:
+		f.write(username + '\n')
+
